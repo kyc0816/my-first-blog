d1c8c5f3ea75b831c4124b24798ee039dad333ee
kyc0816 <youngcheol33@gmail.com>
2019-02-12 19:41:13 +0900

    ServiceLearningAdded

--- blog/views.py
+++ blog/views.py
@@ -88,19 +88,28 @@ def service_learning(request):
 
         # **** dataType = string && REAL CODE
         received_string = request.body.decode('utf-8')      # received_string be like --> ID + Button + Lat(32.2576) + Lon(136.2345)
-        if received_string[0]=='1':                         # Min-Su's ID is 1
-            emer_kid_name = '김민수'
-            emer_kid_parent_email = 'jinrudals135@gmail.com'
-            emer_kid_age = '15'
-            emer_kid_school = '서울중학교'
-            emer_kid_class = '2학년 3반'
-            emer_kid_past_bullied_record = '있음'
+        #Original
+        # if received_string[0]=='1':                         # Min-Su's ID is 1
+        #     emer_kid_name = '김민수'
+        #     emer_kid_parent_email = 'jinrudals135@gmail.com'
+        #     emer_kid_age = '15'
+        #     emer_kid_school = '서울중학교'
+        #     emer_kid_class = '2학년 3반'
+        #     emer_kid_past_bullied_record = '있음'
+
+        emer_kid_name = '김민수'
+        emer_kid_parent_email = 'jinrudals135@gmail.com'
+        emer_kid_age = '15'
+        emer_kid_school = '서울중학교'
+        emer_kid_class = '2학년 3반'
+        emer_kid_past_bullied_record = '있음'
+              
         # GPS lat&lon extraction
         received_lat = received_string[2:9]
         received_lon = received_string[9:]
 
         # email body in common
-        email_body = emer_kid_school + emer_kid_name + ' 학생이 학교 폭력 신고 장치를 통해 도움을 요청하였습니다.\n\n아래 링크를 통해 신고 위치를 확인하십시오.\n\n\n\n' + 'https://www.google.com/maps/search/?api=1&query=' + received_lat + ',' + received_lon + '\n\n\n'
+        email_body = received_string[0] + emer_kid_school + emer_kid_name + ' 학생이 학교 폭력 신고 장치를 통해 도움을 요청하였습니다.\n\n아래 링크를 통해 신고 위치를 확인하십시오.\n\n\n\n' + 'https://www.google.com/maps/search/?api=1&query=' + received_lat + ',' + received_lon + '\n\n\n'
 
         # e-mail to parents
         email_parents = EmailMessage('귀하의 자녀 ' + emer_kid_name + '(으)로부터 도움 요청', email_body, to=[emer_kid_parent_email])
